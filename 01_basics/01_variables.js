09cffce5849d66dfa1dd2ab944fc77278c198531
rohitpanwar291323-lang <rohitpanwar291323@gmail.com>
2026-01-04 20:24:40 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 13423
+let acountEmail = "rpanwar.gmail.com"
+var accountPassword = "19371"
+accountCity = "agra"
+
+//accountId = 22341 // can't change
+// prefer not to use var becuase of scope problem
+
+accountEmail = "rohit@gmail.com"
+accountPassword ="2411"
+accountCity = "delhi"
+
+console.table([accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";// treat all js code as newer version
+
+let name="rohit"
+let age= 20
+let isLoggedIn= false
+let state;
+
+
+//number => 2 to power 53
+//bigint used for very big no.
+//string  =>""
+//boolean => true/false
+//null => standalone value 
+//undefined => not assigned value
+//symbol => unique
+
+//object
+
+console.log(typeof "name");
