35e2db8adff30a518d47f4c6e0bd98e10462dfaa
rohitpanwar291323-lang <rohitpanwar291323@gmail.com>
2026-01-04 21:16:11 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,50 @@
+let score = "45abc"
+
+//console.log(typeof score);
+let valueInNumber= Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33"=> 33
+// "33abc"=> NaN (not a number)
+// true => 1 0=> false
+
+let isLoggedIn = 1
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+
+
+// 1=> true 0=>false
+// ""=> false
+// "rohit"=>true
+
+
+// operations
+let value = 5
+let negValue = -value
+//console.log(negValue)
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(4/2);
+//console.log(4%2);
+
+
+
+
+
+
+
+let str1 = "hello"
+let str2 =" rohit"
+let str3 = str1 + str2
+//console.log(str3)
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+
+
+
