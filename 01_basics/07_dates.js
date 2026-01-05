ec5e177a46b86a203487f27ff0976806adb6a627
rohitpanwar291323-lang <rohitpanwar291323@gmail.com>
2026-01-05 21:39:28 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,39 @@
+const myDate = new Date ()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleTimeString());
+
+//let myCreatedDate = new  Date (2025 , 0 , 6)
+let myCreatedDate = new  Date (2025 , 0 , 6 , 4 , 15 , 22)
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+//console.log(Math.round(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getMonth());
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
