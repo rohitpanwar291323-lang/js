52a2a8bece894e25df42838b0da8caba9f75cf26
rohitpanwar291323-lang <rohitpanwar291323@gmail.com>
2026-01-08 21:11:09 +0000

    lopps in js

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+// let index = 0;
+// while (index <=10) {
+//     console.log(`value of index is ${index}`);
+//     index++;
+// }
+
+let myArray = ["Rohit", "Sumit", "Ankit", "Suresh", "Ajay"];
+
+let index = 0;
+while (index < myArray.length) {
+    console.log(`value is ${myArray[index]}`);
+    index++;
+    
+}
