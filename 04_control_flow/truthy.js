56413c043027ddf333041297aec51928d295db7e
rohitpanwar291323-lang <rohitpanwar291323@gmail.com>
2026-01-08 15:04:24 +0000

    control flow in the js

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,33 @@
+const userGotEmail = []
+
+// if(userGotEmail){
+//     console.log("user has the email");
+    
+// }
+
+// else{
+//     console.log("user dont have the email");
+    
+// }
+
+
+// Falsy values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+
+if(userGotEmail.length === 0){
+    console.log("array is empty");
+    
+}
+
+const emptyObject = {}
+
+if(Object.keys(emptyObject).length === 0){
+    console.log("Object is empty");
+    
+}
+
+ 
